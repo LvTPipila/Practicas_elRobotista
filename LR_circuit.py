f7aeada16567a5a51f58150844272e669af7dc30
Carlos Rodriguez Cortes <rgx220dz@protonmail.com>
2023-09-11 11:35:17 +0200

    Add new script to run solve_ivp

---
+++ LR_circuit.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+# from scipy import signal
+
+def lr_circuit(t, y):
+    r = 0.83
+    l = 2.31e-3
+    vin = 1
+    A = -r/l
+    B = 1./l
+    return A * y + B * vin
+
+
+def main():
+    # Parameters for simulation
+    Vin = 12
+    R = 0.83
+    L = 2.31e-3
+
+    # call the function of LR circuit
+    t0 = 0.
+    tf = 0.05
+    ts = 1e-4
+    t_span = np.arange(t0, tf + ts, ts)
+    sol = solve_ivp(lr_circuit, [0., 0.1], [0.], t_eval=t_span)
+
+    """
+    K = 1/R
+    tau = L/R
+    lti = signal.lti([1/R], [tau, 1])
+    t, y = signal.step(lti)
+    """
+
+    plt.plot(sol.t, sol.y[0])
+    plt.xlabel('Time')
+    plt.ylabel('Response')
+    plt.title('Step reponse 1. order')
+    plt.grid()
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
+
